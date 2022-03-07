e9275e09fc70367cf402184bd3f8fdf49aebfb25
RenceHS <stevestoconing1628@gmail.com>
2022-03-07 11:36:22 +0800

    Update updateItem.h

--- updateItem.h
+++ updateItem.h
@@ -9,6 +9,9 @@ int updateItem()
 
     int chooseItem, flushScan, verifyUNo=0, lengthStringU=0;
     int loopSearchU = 0, loopUpdate=0, detailUpdate=0, loopAnotherUpdate=0, loopVerifyU=0;
+    char dataYr[255], dataMo[255], dataDay[255];
+    int dataMoI, dataDayI;
+    int mmHolder, ddHolder;
     char verifyU[2], update[2];
     char userInX[32];
 	char userInY[32];
@@ -54,29 +57,131 @@ int updateItem()
                     {
                         case 0:
                             printf("---Updating Description---\n");
-                            printf("Enter Data: ");
+                            printf("Enter Description: ");
                             scanf("%[^\n]s", dataIn);
                             //printf("%s\n", dataIn);
                             setDes(sampleU, dataIn);
                             break;
                         case 1:
                             printf("---Updating Quantity---\n");
-                            printf("Enter Data: ");
-                            scanf("%[^\n]s", dataIn);
+                            printf("Enter Quantity: ");
+                            scanf("%[0-9]s", dataIn);
                             //printf("%s\n", dataIn);
                             setQuantity(sampleU, dataIn);
                             break;
                         case 2:
                             printf("---Updating Expiry Date---\n");
-                            printf("Enter Data: ");
-                            scanf("%[^\n]s", dataIn);
+                            printf("Enter Product Expiry(YYYY/MM/DD):\n");
+                            while (scanDate == true)
+                            {
+                                printf("Year: ");
+                                gets(dataYr);
+                                check = 0;
+                                for (x = 0; x != 4; x++)
+                                {
+                                    if ((isdigit(dataYr[x]) == 0) || strlen(dataYr) != 4 || (dataYr == '\0')|| strlen(dataYr) <= 0)
+                                    {
+                                        printf("Invalid year, please try again.\n");
+                                        break;
+                                    }
+
+                                    else
+                                    {
+                                        check = check + 1;
+                                    }
+
+                                }
+
+                                if (check == 4)
+                                {
+                                    while (scanDate == true)
+                                    {
+                                        printf("Month: ");
+                                        gets(dataMoI);
+                                        mmHolder = atoi(dataMoI);
+                                        for (x = 0; x != 2; x++)
+                                        {
+                                            if ((isdigit(dataMo[x]) == 0) || strlen(dataMo) < 2 || (mmHolder > 12))
+                                            {
+                                                printf("Invalid Month, please try again.\n");
+                                                break;
+                                            }
+
+                                            else
+                                            {
+                                            }
+                                        }
+
+                                        if ((strlen(dataMo) == 2) && (mmHolder >= 1) && (mmHolder <= 12))
+                                        {
+                                            while (scanDate == true)
+                                            {   
+                                                printf("Date: ");
+                                                gets(dataDay);
+                                                ddHolder = atoi(dataDay);
+                                                check = 0;
+                                                for (x = 0; x != 2; x++)
+                                                {
+                                                    if ((isdigit(dataDay[x]) == 0) || (strlen(dataDay) < 2) || (ddHolder > 31))
+                                                    {
+                                                        printf("Invalid Date, please try again.\n");
+                                                        break;
+                                                    }
+
+                                                    else
+                                                    {
+                                                    }
+                                                }
+
+                                                if ((strlen(dataDay) == 2) && (ddHolder >= 1 && ddHolder <= 31) && (mmHolder == 1 || mmHolder == 3 || mmHolder == 5 || mmHolder == 7 || mmHolder == 8 || mmHolder == 10 || mmHolder == 12))
+                                                {
+                                                    printf("Date is valid.\n");
+                                                    break;
+                                                }
+
+                                                else if ((strlen(dataDay) == 2) && (ddHolder >= 1 && ddHolder <= 30) && (mmHolder == 4 || mmHolder == 6 || mmHolder == 9 || mmHolder == 11))
+                                                {
+                                                    printf("Date is valid.\n");
+                                                    break;
+                                                }
+
+                                                else if ((strlen(dataDay) == 2) && (ddHolder >= 1 && ddHolder <= 28) && (mmHolder == 2))
+                                                {
+
+                                                    printf("Date is valid.\n");
+                                                    break;
+                                                }
+
+                                                else if ((strlen(dataDay) == 2) && (ddHolder == 29 ) && (mmHolder == 2) && ((yyHolder % 400 == 0) || (yyHolder % 4 == 0) && (yyHolder % 100 != 0)))
+                                                {
+
+                                                    printf("Date is valid (Leap Year).\n");
+                                                    break;
+                                                }
+
+                                            }
+                                            break;
+                                        }
+
+                                        else
+                                        {
+                                        }
+                                    }
+                                    break;
+                                }
+
+                                else
+                                {
+
+                                }
+                            }   
                             //printf("%s\n", dataIn);
                             setExp(sampleU, dataIn);
                             break;
                         case 3:
                             printf("---Updating Price---\n");
                             printf("Enter Data: ");
-                            scanf("%[^\n]s", dataIn);
+                            scanf("%[0-9,.]s", dataIn);
                             //printf("%s\n", dataIn);
                             setPrice(sampleU, dataIn);
                             break;
